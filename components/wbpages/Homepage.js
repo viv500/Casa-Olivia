3f2c836b9df5b442a4fcdcf92eca1878e1f34789
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2024-01-13 14:30:22 -0500

    Add files via upload

---
+++ components/wbpages/Homepage.js
@@ -0,0 +1,16 @@
+import "../../App.css"
+import Handle from '../Handle.js'
+import React from 'react'
+import Widget from '../Widget.js'
+
+
+function Homepage () {
+    return (
+        <>
+        <Handle />
+    
+        </>
+    )
+}
+
+export default Homepage
