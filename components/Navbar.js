bf77393d709917fdb168d22631dd38242b940319
Vivek Jude Manoj <149888120+viv500@users.noreply.github.com>
2024-01-13 19:18:30 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            VJ DINER <i className='fab fa-typo3' />
+            CASA OLIVIA <i className='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
